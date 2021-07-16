a736291d4827fd38a66866a5f3646c5c7ea4a6ab
cliomaas <cliomaas@gmail.com>
2021-07-16 18:21:55 -0300

    projeto landing page desktop concluido

---
+++ javascript/scripts.js
@@ -0,0 +1,62 @@
+var slideIndex = 1;
+
+var myTimer;
+
+var slideshowContainer;
+
+window.addEventListener("load", function () {
+    showSlides(slideIndex);
+    myTimer = setInterval(function () { slides(1) }, 4000);
+})
+
+// BOTOES - CONTROLE
+function slides(n) {
+    clearInterval(myTimer);
+    if (n < 0) {
+        showSlides(slideIndex -= 1);
+    } else {
+        showSlides(slideIndex += 1);
+    }
+
+    if (n === -1) {
+        myTimer = setInterval(function () { slides(n + 2) }, 3000);
+    } else {
+        myTimer = setInterval(function () { slides(n + 1) }, 3000);
+    }
+}
+
+
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+}
+
+pause = () => {
+    clearInterval(myTimer);
+}
+
+resume = () => {
+    clearInterval(myTimer);
+    myTimer = setInterval(function () { plusSlides(slideIndex) }, 3000);
+}
+
+
+function setCredentials(event) {
+    event.preventDefault();
+    var inputEmail = document.getElementById("email");
+    localStorage.setItem("email", inputEmail.value);
+
+
+}
